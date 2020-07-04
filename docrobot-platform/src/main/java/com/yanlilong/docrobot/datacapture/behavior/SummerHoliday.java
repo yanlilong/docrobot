b4bfa7b69d944382ab71ab511c7aee012bd4157f
yanlilong <yan.w.li@outlook.com>
2020-07-04 10:07:38 +0200

    change context config and delete some unuseful classes

--- docrobot-platform/src/main/java/com/yanlilong/docrobot/datacapture/behavior/SummerHoliday.java
+++ docrobot-platform/src/main/java/com/yanlilong/docrobot/datacapture/behavior/SummerHoliday.java
@@ -1,4 +1,24 @@
 package com.yanlilong.docrobot.datacapture.behavior;
 
+import java.util.Date;
+
 public class SummerHoliday {
+    private Date startDate;
+    private Date finishDate;
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
 }
